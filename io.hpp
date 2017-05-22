c5f89b63d5d403da9809e5eb761ebbaad34fff9c
Vector2025 <vector2025@gmail.com>
2017-05-22 20:05:38 +0300

    Update io.hpp

--- io.hpp
+++ io.hpp
@@ -1,13 +1,3 @@
-//          Copyright Louis Delacroix 2010 - 2014.
-// Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
-//          http://www.boost.org/LICENSE_1_0.txt)
-//
-// A pretty printing library for C++
-//
-// Usage:
-// Include this header, and operator<< will "just work".
-
 #ifndef H_PRETTY_PRINT
 #define H_PRETTY_PRINT
 
