ea8c3869978c246a1ee57acd68c773847b593875
Bara Tudor <vector2025@gmail.com>
2017-06-01 01:18:43 +0300

    de ce nu

--- hask/list.hpp
+++ hask/list.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 #include <iosfwd>   // for stream operators
 #include <stddef.h> // for ptrdiff_t
@@ -7,121 +6,49 @@
 namespace hask
 {
 
-    namespace detail
-    {
-        template <typename T>
-        class ConstListIterator {
-        public:
-            using iterator_category = std::forward_iterator_tag;
-            using value_type        = T;
-            using const_reference   = const T&;
-            using const_pointer     = const T*;
-            using node              = struct node<T>;
-            using difference_type   = ptrdiff_t;
-            using self              = ConstListIterator<T>;
-
-            node* node_;
-
-            explicit ConstListIterator() : node_() { }
-            explicit ConstListIterator(node* node_) : node_(node_) { }
-
-            reference
-            operator*() noexcept
-            {
-                return node_->data;
-            }
-
-            pointer
-            operator->() noexcept
-            {
-                return &node_->data;
-            }
-
-            self&
-            operator++() noexcept
-            {
-                node_ = node_->next;
-                return *this;
-            }
-
-            self
-            operator++(int) noexcept
-            {
-                auto tmp = *this;
-                node_ = node_->next;
-                return tmp;
-            }
-
-            friend bool
-            operator==(const self& lhs, const self& rhs) noexcept
-            {
-                return (lhs.node_ == rhs.node_);
-            }
-
-            friend bool
-            operator!=(const self& lhs, const self& rhs) noexcept
-            {
-                return (lhs.node_ != rhs.node_);
-            }
-        };
-    } // namespace detail
-
     template <typename T>
     class List {
 
     public:
-        using self            = const List<T>&;
         using value_type      = T;
         using size_type       = size_t;
-        using const_reference = const T&;
-        using const_iterator  = detail::ConstListIterator<T>;
 
     private:
 
         struct node {
             T data;
-            std::unique_ptr<T> next;
-            node()
-                : next(nullptr)
-            node(T data)
+            std::shared_ptr<T> next;
+            node(T data, std::shared_ptr<T> next)
                 : data(data)
-                , next(nullptr)
-            node(T data, std::unique_ptr<T> next, node* prev)
-                : data(data)
+                , next(next) { }
         };
 
-        std::unique_ptr<node> root = get_node();
-        size_type size_ = 0;
+        std::shared_ptr<node> root;
+
+        List(value_type data, List tail)
+            : root(std::make_shared<node>(data, tail->root)) { }
 
     public:
 
-        friend value_type head(self list)
+        friend value_type head(List list)
         {
             return root->data;
         }
-        
-        friend self tail(self list)
+
+        friend List tail(List list)
         {
             return root->next;
         }
 
-        bool
-        empty() const noexcept
-        { return size_ == 0; }
-
-        size_type
-        size() const noexcept
-        { return size_; }
-
-        friend std::ostream& operator<<(std::ostream& stream, List<T>& _this)
+        friend List cons(value_type data, List tail)
         {
-            for(auto e: _this){
-                stream << e << " ";
-            }
+            return List(data, tail);
         }
 
+        bool empty()
+        {
+            return !root;
+        }
     };
 
-    private:
-        void
 }
